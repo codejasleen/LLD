322734ce6ee9f0d7d032e9bf89e16a20d3943a14
codejasleen <jsleeno7@gmail.com>
2026-03-01 00:20:23 +0530

    Added ISP Ex8

---
+++ SOLID/Ex8/src/SecretaryTool.java
@@ -0,0 +1,7 @@
+public class SecretaryTool implements MinutesAdmin {
+    private final MinutesBook book;
+    public SecretaryTool(MinutesBook book) { this.book = book; }
+
+    @Override public void addMinutes(String text) { book.add(text); }
+
+}
